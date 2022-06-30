5645d4cc02f8f92c4dc3f938f231e659d2a034f8
Kamil Michalski <kmichalski@outlook.com>
2022-06-30 14:17:39 +0200

    clean up

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 import './style.css'
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { doc, addDoc, collection, deleteDoc, getDoc, getDocs, getFirestore } from "firebase/firestore";
 import { firebaseConfig } from './config';
 import { deleteGame } from './games';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
