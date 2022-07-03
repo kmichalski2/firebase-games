a26ecd859b5d702b26677fa80faa8043bf9e3f26
Kamil Michalski <kmichalski@outlook.com>
2022-07-03 11:49:10 +0200

    firebase hosting set up

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html')
+      }
+    }
+  }
+})
